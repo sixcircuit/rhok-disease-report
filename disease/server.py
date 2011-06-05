0b4465a02af64fc7b0cabc29d3a3306ce1f143ff
Kendrick Taylor <kendrick@chickrx.com>
2011-06-05 00:32:38 +0000

    added handlers

--- disease/server.py
+++ disease/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os
 import tornado.ioloop
 import tornado.web
@@ -12,13 +14,19 @@ class IndexHandler(tornado.web.RequestHandler):
   def get(self):
     self.render("index.html")
 
+class DiseaseReportHandler(tornado.web.RequestHandler):
+  def post(self):
+    print(self.request.arguments)    
+
 
 application = tornado.web.Application([
   (r"/", IndexHandler),
-  (r"/index", IndexHandler)
+  (r"/index", IndexHandler),
+  (r"/report", DiseaseReportHandler)
 ], **settings)
 
 if __name__ == "__main__":
   application.listen(80)
+  print("Server listening on all addresses, port 80")
   tornado.ioloop.IOLoop.instance().start()
 
